c7cb906ffcb9dbf744b73db3a8adfb2e624291f4
unknown <pawanrajora290@gmail.com>
2022-11-08 17:31:55 +0530

    pelondrom

---
+++ palendrom/pelodrom.js.js
@@ -0,0 +1,35 @@
+let palendrom = 'noon'
+
+
+let index = 0;
+let len = palendrom.lenght
+let store = palendrom.split('')
+
+console.log(store);
+
+let store2 = store.reverse()
+
+
+console.log(store2);
+
+
+let store3 = store.join('')
+console.log(store3)
+
+
+    
+    if(palendrom == store3){
+        
+        console.log('its palendrom')
+        
+    }
+    
+    else{
+        
+        console.log('its not a palendrom')
+        
+    }
+    
+ 
+    
+
